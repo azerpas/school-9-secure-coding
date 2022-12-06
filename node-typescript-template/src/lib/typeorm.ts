8208c74af8f8fada3834377882ffcde5eae76a67
azerpas <xskylix@hotmail.fr>
2022-12-06 17:53:19 +0100

    test: added first tests for user

--- node-typescript-template/src/lib/typeorm.ts
+++ node-typescript-template/src/lib/typeorm.ts
@@ -4,17 +4,18 @@ import 'dotenv/config'
 
 export const getAppDataSource = (): DataSource => {
     if (!process.env.POSTGRES_HOST) throw new Error("POSTGRES_HOST is not defined")
-    else if (!process.env.POSTGRES_PORT) throw new Error("POSTGRES_PORT is not defined")
-    else if (!process.env.POSTGRES_USER) throw new Error("POSTGRES_USER is not defined")
-    else if (!process.env.POSTGRES_PASSWORD) throw new Error("POSTGRES_PASSWORD is not defined")
-    else if (!process.env.POSTGRES_DB) throw new Error("POSTGRES_DB is not defined")
+    if (!process.env.POSTGRES_PORT) throw new Error("POSTGRES_PORT is not defined")
+    if (!process.env.POSTGRES_TEST_PORT) throw new Error("POSTGRES_TEST_PORT is not defined")
+    if (!process.env.POSTGRES_USER) throw new Error("POSTGRES_USER is not defined")
+    if (!process.env.POSTGRES_PASSWORD) throw new Error("POSTGRES_PASSWORD is not defined")
+    if (!process.env.POSTGRES_DB) throw new Error("POSTGRES_DB is not defined")
     else return new DataSource({
         type: "postgres",
         host: process.env.POSTGRES_HOST,
-        port: parseInt(process.env.POSTGRES_PORT),
+        port: process.env.NODE_ENV === `test` ?  parseInt(process.env.POSTGRES_TEST_PORT) : parseInt(process.env.POSTGRES_PORT),
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        database: process.env.NODE_ENV === `test` ? `${process.env.POSTGRES_DB}-test` : process.env.POSTGRES_DB,
+        database: process.env.POSTGRES_DB,
         synchronize: true,
         logging: true,
         entities: [User],
---
+++ node-typescript-template/src/specs/entities/user.spec.ts
@@ -0,0 +1,46 @@
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+import { User } from '@entities/index'
+import { getAppDataSource } from '@lib/typeorm'
+import { DataSource, QueryFailedError } from 'typeorm'
+import { expect } from 'chai'
+
+chai.use(chaiAsPromised)
+
+describe('User', function () {
+    let datasource: DataSource
+
+    before(async function () {
+        // TODO: initialise the datasource (database connection)
+        datasource = await getAppDataSource().initialize()
+    })
+
+    beforeEach(async function () {
+        // TODO: drop the content of the user table between each it().
+        await datasource.getRepository(User).clear()
+    })
+
+    describe('validations', function () {
+        it('should create a new User in database', async () => {
+            await datasource
+                .getRepository(User)
+                .save({
+                    email: "",
+                    passwordHash: "", 
+                    firstName: "hello", 
+                    lastName: "world", 
+                    id: 0, 
+                    emanpm: 0
+                } as User)
+            const user = await datasource.getRepository(User).findOne({where: {firstName: "hello"}})
+            expect(user).to.not.be.null
+            expect(user?.firstName).to.equal("hello")
+        })
+
+        it('should raise error if email is missing', async function () {
+            // hint to check if a promise fails with chai + chai-as-promise:
+            // const promise = datasource.getRepository(User).save()
+            // await chai.expect(promise).to.eventually.be.rejectedWith(QueryFailedError, "message")
+        })
+    })
+})
