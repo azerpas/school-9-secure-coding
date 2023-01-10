00af7e32407fbe33e52081ed04804903a832e3c4
azerpas <xskylix@hotmail.fr>
2023-01-10 11:46:01 +0100

    feat(db): caching datasource

--- tp/src/specs/entities/user.spec.ts
+++ tp/src/specs/entities/user.spec.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
 import { User } from '@entities/index'
-import { getAppDataSource } from '@lib/typeorm'
+import { getAppDataSourceInitialized } from '@lib/typeorm'
 import { DataSource, QueryFailedError } from 'typeorm'
 import { expect } from 'chai'
 // import { ValidationError } from 'class-validator'
@@ -14,7 +14,7 @@ describe('User', function () {
 
     before(async function () {
         // TODO: initialise the datasource (database connection)
-        datasource = await getAppDataSource().initialize()
+        datasource = await getAppDataSourceInitialized()
     })
 
     beforeEach(async function () {
