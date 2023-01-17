6d9514c2200800423db610300776748803e905ac
azerpas <xskylix@hotmail.fr>
2023-01-17 16:29:26 +0100

    refactor: json schemas

---
+++ tp/src/schemas/json/index.ts
@@ -0,0 +1,5 @@
+import * as createSessionRequestBody from './create-session-request-body.json'
+import * as createUserRequestBody from './create-user-request-body.json'
+import * as createUserResponseBody from './create-user-response-body.json'
+
+export { createSessionRequestBody, createUserRequestBody, createUserResponseBody }
