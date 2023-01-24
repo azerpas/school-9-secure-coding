c943f735f7c96ed327f322e350163b203fe73cdd
azerpas <xskylix@hotmail.fr>
2023-01-24 18:21:54 +0100

    test(fastify): status >= 500 and dev mode

--- tp/src/specs/lib/fastify.spec.ts
+++ tp/src/specs/lib/fastify.spec.ts
@@ -1,7 +1,7 @@
-import { assertsResponseSchemaPresenceHook } from '@lib/fastify'
+import { assertsResponseSchemaPresenceHook, server } from '@lib/fastify'
 import { expect } from 'chai'
 import fastify from 'fastify'
-describe('assertsResponseSchemaPresenceHook', () => {
+describe('Fastify general errors', () => {
     it('Should throw an error when schema is missing', () => {
         const testServer = fastify()
         testServer.addHook('onRoute', assertsResponseSchemaPresenceHook)
@@ -14,5 +14,23 @@ describe('assertsResponseSchemaPresenceHook', () => {
                 }
             })
         }).to.throw()
+    })
+
+    it('should fetch the error if statusCode >= 500 and dev mode', async () => {
+        // add a route that throws an error to `server`
+        server.route({
+            method: 'GET',
+            url: '/error',
+            handler: () => {
+                throw new Error('MyCustomError')
+            }
+        })
+        const response = await server.inject({ url: '/error', method: 'GET' })
+        expect(response.statusCode).to.equal(500)
+        if (process.env.NODE_ENV === 'production') {
+            expect(response.payload).to.equal('{"error":"Internal Server Error"}')
+        } else {
+            expect(response.payload).to.equal('{"error":"MyCustomError"}')
+        }
     })
 })
