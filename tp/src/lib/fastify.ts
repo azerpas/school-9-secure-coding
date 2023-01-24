091c915f0720bc8e3166148b2d928626c0f57a6f
azerpas <xskylix@hotmail.fr>
2023-01-24 17:59:36 +0100

    feat(hooks): onRoute schema hook and error handler

--- tp/src/lib/fastify.ts
+++ tp/src/lib/fastify.ts
@@ -2,6 +2,7 @@ import fastify, { RouteOptions } from 'fastify'
 import cookie, { FastifyCookieOptions } from '@fastify/cookie'
 // import { createSessionRequestBody, createUserRequestBody, createUserResponseBody } from '@schemas/json'
 import { userRoutes } from '@routes/users'
+import { checkSchemaBodyQueryParamsHook, MissingValidationSchemaError } from '@hooks/index'
 
 export const server = fastify(
     {
@@ -14,14 +15,15 @@ export const server = fastify(
         }
     }
 )    // TODO: replace with a real secret dotenv variable
-    .register(cookie, {secret: 'my-secret'} as FastifyCookieOptions)
+    .register(cookie, { secret: 'my-secret' } as FastifyCookieOptions)
     .register(userRoutes, { prefix: '/users' })
     .decorateRequest('session', null)
-    // .addHook('preHandler', loadSession)
-
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
-})
+    .addHook('onRoute', checkSchemaBodyQueryParamsHook)
+    .setErrorHandler((error, request, reply) => {
+        if (error instanceof MissingValidationSchemaError) {
+            void reply.status(400).send({ error: error.message })
+        }
+    })
 
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
@@ -31,6 +33,7 @@ server.listen({ port: 8080 }, (err, address) => {
     console.log(`Server listening at ${address}`)
 })
 
+// Code Legacy used for Exercise 4 reference
 export function assertsResponseSchemaPresenceHook(routeOptions: RouteOptions) {
     if (!routeOptions.schema) {
         throw new Error('Missing schema for this route')
