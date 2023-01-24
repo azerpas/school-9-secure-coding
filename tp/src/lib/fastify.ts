443fad76983c6c5e68d3003b4a2eaef10ff3fbcf
layfredrc <frederic.lay@efrei.net>
2023-01-25 00:42:25 +0100

    feat(fastify): errorHandler hook + test

--- tp/src/lib/fastify.ts
+++ tp/src/lib/fastify.ts
@@ -1,38 +1,59 @@
-import fastify, { RouteOptions } from 'fastify'
+import fastify, {
+    RouteOptions,
+    FastifyError,
+    FastifyReply,
+    FastifyRequest,
+} from 'fastify'
 import cookie, { FastifyCookieOptions } from '@fastify/cookie'
 // import { createSessionRequestBody, createUserRequestBody, createUserResponseBody } from '@schemas/json'
 import { userRoutes } from '@routes/users'
-import { checkSchemaBodyQueryParamsHook, MissingValidationSchemaError } from '@hooks/index'
+import {
+    checkSchemaBodyQueryParamsHook,
+    MissingValidationSchemaError,
+} from '@hooks/index'
+import { ValidationError } from 'class-validator'
+import { EntityNotFoundError } from 'typeorm'
 
-export const server = fastify(
-    {
-        logger: true,
-        ajv: {
-            customOptions: {
-                removeAdditional: false,
-                useDefaults: false
-            },
-        }
+export const errorHandlerHook = (
+    error: FastifyError,
+    req: FastifyRequest,
+    reply: FastifyReply
+) => {
+    if (error instanceof MissingValidationSchemaError) {
+        void reply.status(400).send({ error: error.message })
     }
-)    // TODO: replace with a real secret dotenv variable
+    if (process.env.NODE_ENV === 'production' && reply.statusCode >= 500) {
+        void reply.status(500).send({ error: 'Internal Server Error' })
+    } else {
+        void reply.status(500).send({ error: error.message })
+    }
+    if (error instanceof ValidationError) {
+        return reply.status(400).send({ error: error.constraints })
+    }
+    if (error instanceof EntityNotFoundError) {
+        return reply.status(404).send({ error: error.message })
+    }
+}
+
+export const server = fastify({
+    logger: true,
+    ajv: {
+        customOptions: {
+            removeAdditional: false,
+            useDefaults: false,
+        },
+    },
+}) // TODO: replace with a real secret dotenv variable
     .register(cookie, { secret: 'my-secret' } as FastifyCookieOptions)
     .register(userRoutes, { prefix: '/users' })
     .decorateRequest('session', null)
     .addHook('onRoute', checkSchemaBodyQueryParamsHook)
-    .setErrorHandler((error, request, reply) => {
-        if (error instanceof MissingValidationSchemaError) {
-            void reply.status(400).send({ error: error.message })
-        }
-        if (process.env.NODE_ENV === 'production' && reply.statusCode >= 500) {
-            void reply.status(500).send({ error: 'Internal Server Error' })
-        } else {
-            void reply.status(500).send({ error: error.message })
-        }
-    })
+    .addHook('onRoute', assertsResponseSchemaPresenceHook)
+    .setErrorHandler(errorHandlerHook)
 
 // Code Legacy used for Exercise 4 reference
 export function assertsResponseSchemaPresenceHook(routeOptions: RouteOptions) {
     if (!routeOptions.schema) {
-        throw new Error('Missing schema for this route')
+        throw new Error(`Missing schema for this route ${routeOptions.url}`)
     }
 }
--- tp/src/specs/lib/fastify.spec.ts
+++ tp/src/specs/lib/fastify.spec.ts
@@ -1,36 +1,65 @@
-import { assertsResponseSchemaPresenceHook, server } from '@lib/fastify'
+import {
+    assertsResponseSchemaPresenceHook,
+    server,
+    errorHandlerHook,
+} from '@lib/fastify'
 import { expect } from 'chai'
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
+
 describe('Fastify general errors', () => {
-    it('Should throw an error when schema is missing', () => {
-        const testServer = fastify()
-        testServer.addHook('onRoute', assertsResponseSchemaPresenceHook)
-        expect(() => {
-            testServer.route({
-                method: 'GET',
-                url: '/',
-                handler: () => {
-                    return
+    it('Should throw an error when response schema is missing', () => {
+        const unsafeRoute = async (route: FastifyInstance) => {
+            route.post(
+                '/un-safe',
+                {
+                    schema: {
+                        body: {
+                            properties: {
+                                foo: 'bar',
+                            },
+                        },
+                    },
+                },
+                () => true
+            )
+            const testServer = fastify()
+                .setErrorHandler(errorHandlerHook)
+                .addHook('onRoute', assertsResponseSchemaPresenceHook)
+                .register(unsafeRoute)
+            await expect(testServer).to.eventually.be.rejected.and.deep.include(
+                {
+                    statusCode: 500,
+                    name: 'Internal Server Error',
+                    message: 'Response schema not found for route /un-safe',
                 }
-            })
-        }).to.throw()
+            )
+        }
     })
 
     it('should fetch the error if statusCode >= 500 and dev mode', async () => {
         // add a route that throws an error to `server`
-        server.route({
-            method: 'GET',
+        const errorRoute = async (route: FastifyInstance) => {
+            route.get('/error', () => {
+                throw {
+                    statusCode: 500,
+                    message: 'MyCustomError',
+                }
+            })
+        }
+
+        const testServer = fastify()
+            .setErrorHandler(errorHandlerHook)
+            .register(errorRoute)
+
+        const response = await testServer.inject({
             url: '/error',
-            handler: () => {
-                throw new Error('MyCustomError')
-            }
+            method: 'GET',
         })
-        const response = await server.inject({ url: '/error', method: 'GET' })
-        expect(response.statusCode).to.equal(500)
+        expect(response.statusCode).equal(500)
         if (process.env.NODE_ENV === 'production') {
-            expect(response.payload).to.equal('{"error":"Internal Server Error"}')
+            expect(response.payload).equal('Internal Server Error')
         } else {
-            expect(response.payload).to.equal('{"error":"MyCustomError"}')
+            expect(response.payload).equal('{"error":"MyCustomError"}')
         }
     })
 })
