307c663378a1fc0de10c8c4a3fa9e2744ea7ddb3
Azerpas <manikhouth.anthony@gmail.com>
2023-01-26 16:39:12 +0100

    refactor: updated create session interface

--- tp/src/schemas/types/create-session-request-body.d.ts
+++ tp/src/schemas/types/create-session-request-body.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export interface CreateSessionResponseBody {
+export interface CreateSessionRequestBody {
   email: string;
   password: string;
 }
