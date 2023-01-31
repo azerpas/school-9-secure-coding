f664a7a3daeef0735e12093f74b8868537d91f8a
azerpas <xskylix@hotmail.fr>
2023-01-31 14:17:15 +0100

    refactor: moved session logic from routes to lib

--- tp/src/lib/session.ts
+++ tp/src/lib/session.ts
@@ -1,22 +1,39 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { request } from 'http'
 import { Session } from '../entities/session'
-import { User } from '../entities/user'
+import { IncorrectPassword, User } from '../entities/user'
+import { getAppDataSourceInitialized } from './typeorm'
 
 declare module 'fastify' {
-  interface FastifyRequest {
-    session?: Session | null
-    user?: User | null
-  }
+    interface FastifyRequest {
+        session?: Session | null
+        user?: User | null
+    }
+}
+
+
+export async function saveSession(reply: FastifyReply, user: User) {
+    const datasource = await getAppDataSourceInitialized()
+
+    const session = datasource.getRepository(Session).create()
+    session.user = user
+
+    await datasource.getRepository(Session).save(session)
+
+    return reply.setCookie('session', session.token, {
+        path: '/',
+        signed: true,
+        domain: 'localhost',
+        httpOnly: true,
+    })
 }
 
 /**
- * The loadSession function is a preHandler hook that can be registered globally on a fastify instance. If you set it up as a global hook, it should do nothing if the session cookie is missing in request. Else, it will decorate the request object with additional user and session properties. For performances reasons, use decorateRequest.
+ * The loadSession function is a preHandler hook that can be registered globally on a fastify instance. 
+ * If you set it up as a global hook, it should do nothing if the session cookie is missing in request. 
+ * Else, it will decorate the request object with additional user and session properties. 
+ * For performances reasons, use decorateRequest.
  */
-export async function saveSession(reply: FastifyReply, user: User) {
-    // TODO: move here the code you already implemented
-    // about session creation and cookie assignment.
-}
-
 export async function loadSession(request: FastifyRequest) {
     // TODO: read the cookie from request.cookies[COOKIE_NAME].
     // TODO: unsign the cookie (or reject if invalid) and retreive the token.
--- tp/src/routes/sessions.ts
+++ tp/src/routes/sessions.ts
@@ -1,11 +1,9 @@
 import {
-    EmailNotFound,
     IncorrectPassword,
-    Session,
     User,
     UserNotFound,
 } from '@entities/index'
-import { PasswordDoesNotMatch } from '@lib/password'
+import { saveSession } from '@lib/session'
 import { getAppDataSourceInitialized } from '@lib/typeorm'
 import { createSessionRequestBody } from '@schemas/json'
 import { CreateSessionRequestBody } from '@schemas/types'
@@ -20,27 +18,16 @@ export async function sessionRoutes(fastify: FastifyInstance) {
             const datasource = await getAppDataSourceInitialized()
             const user = await datasource
                 .getRepository(User)
-                .findOneBy({ email: request.body.email })
+                .findOneBy({ email: request.body.email  })
             if (!user) throw new UserNotFound('User could not be found')
 
             const isValidPassword = await user.isPasswordValid(
-                request.body.password
+                request.body.password 
             )
-
             if (!isValidPassword)
                 throw new IncorrectPassword('Password is incorrect')
 
-            const session = datasource.getRepository(Session).create()
-            session.user = user
-
-            await datasource.getRepository(Session).save(session)
-
-            return reply.setCookie('session', session.token, {
-                path: '/',
-                signed: true,
-                domain: 'localhost',
-                httpOnly: true,
-            })
+            return saveSession(reply, user)
         },
     })
 }
