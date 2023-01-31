e1d95ecf74192d78e8528f3a8c972964a4789c3f
azerpas <xskylix@hotmail.fr>
2023-01-31 14:42:19 +0100

    feat: token property is unique

--- tp/src/entities/session.ts
+++ tp/src/entities/session.ts
@@ -5,17 +5,21 @@ import {
     Entity,
     ManyToOne,
     PrimaryGeneratedColumn,
+    Unique,
 } from 'typeorm'
 import { User } from './'
 import * as crypto from 'crypto'
 import { IsNotEmpty } from 'class-validator'
+import { UniqueInColumn } from 'decorators'
 
 @Entity()
+@Unique(['token'])
 export class Session {
     @PrimaryGeneratedColumn('uuid')
     id!: string
 
     @Column({ length: 384 })
+    @UniqueInColumn({ message: 'Session.token is not unique' })
     token!: string
 
     @ManyToOne(() => User, (user) => user.sessions, { nullable: false })
