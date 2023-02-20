e07fb638035cdfa8c746aac519a5184862e715ea
Azerpas <xskylix@hotmail.fr>
2023-02-20 09:29:17 +0100

    chore: Monitor through Grafana and Kubernetes support (#23)
    * monitoring-bootstrap
    
    * fix: port grafana
    
    * fix: port docker compose
    
    * things working
    
    * ci: init
    
    * ci: changed branch
    
    * ci: fixed default directory
    
    * ci: fixed cache dependency
    
    * fix(ci): fixed path to cache package lock
    
    * fix(ci): removed duplicate path
    
    * ci: f the cache
    
    * fix(ci): ...
    
    * fix(ci): ...
    
    * ci: mv test ci-test
    
    * fix(ci): fixed run
    
    * fix(ci): date set
    
    * fix(ci): date tag format
    
    * fix(ci): working directory for build
    
    * fix(ci): username variable docker
    
    * fix(ci): typo var
    
    * fix(ci): added context for username var
    
    * ci: updated unit tests with db checks
    
    * Update cicd.yml
    
    * fix(ci): env secrets
    
    * fix(ci): missing POSTGRES_DB
    
    * fix(ci): Dockerfile wrong command
    
    * ci update for connecting to gcp and deploy test service
    
    * use credentials
    
    * fix branches ci
    
    * reset to base
    
    * fix
    
    * fix
    
    * fix 2
    
    * fix 3
    
    * fix 4
    
    * push and pull to registry
    
    * use tag
    
    * remove dependency
    
    * fix
    
    * fix
    
    * fix
    
    * fix repository
    
    * fix repo
    
    * add docker auth
    
    * deploy-db
    
    * deploy db
    
    * deploy db
    
    * deploy db
    
    * deploy db
    
    * add docker-compose-production
    
    * file
    
    * rebase and docker prod
    
    * use kompose
    
    * use kompose
    
    * use kompose
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * add loadbalancing type
    
    * Update cicd.yml
    
    * change dockerfile port
    
    * add healthcheck and remove links
    
    * Update cicd.yml
    
    * test
    
    * test
    
    * test webapp
    
    * remove network test
    
    * Update cicd.yml
    
    * Update cicd.yml
    
    * Update cicd.yml
    
    * Update cicd.yml
    
    * add script to remove network
    
    * update ci
    
    * fix
    
    * add execution permission
    
    * fix script
    
    * remove comment
    
    * retrieve ip or timeout
    
    * test
    
    * change set-env to export
    
    * fix
    
    * deploy db script
    
    * give permission
    
    * add sql service
    
    * add sql variable
    
    * remove useless mount
    
    * git status
    
    * add fastify
    
    * add dependance
    
    * needs
    
    * add environment variable
    
    * remove wait
    
    * grafana
    
    * fix docker compose
    
    * fix
    
    * docs: OWASP
    
    ---------
    
    Co-authored-by: layfredrc <frederic.lay@efrei.net>
    Co-authored-by: Priestley DECELIAN <priestley.decelian@efrei.net>
    Co-authored-by: LeyDece <65225782+LeyDece@users.noreply.github.com>

---
+++ tp/kubernetes/cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+parent_name=$(basename $(dirname $(pwd)))
+
+rm -rf $parent_name-default-networkpolicy.yaml
+
+word="io.kompose.network"
+
+# Loop through all files in the current directory
+for file in *; do
+  # Find the line number of the occurrence of the word
+  line_number=$(grep -n "$word" "$file" | awk -F: '{print $1}')
+
+  # Delete the line if it was found
+  if [ -n "$line_number" ]; then
+    sed -i "${line_number}d" "$file"
+    echo "The line containing '$word' has been deleted from $file."
+  else
+    echo "The word '$word' was not found in $file."
+  fi
+done
--- tp/src/index.ts
+++ tp/src/index.ts
@@ -8,6 +8,7 @@ void (async () => {
     console.log('hello world')
     if (!process.env.FASTIFY_PORT || process.env.FASTIFY_PORT === '') throw new Error('FASTIFY_PORT is not defined')
     const FASTIFY_PORT = parseInt(process.env.FASTIFY_PORT)
+    console.log(`Connecting to database ${process.env.POSTGRES_DB} on ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`);
     const datasource: DataSource = await getAppDataSource().initialize()
     await server.listen({ port: FASTIFY_PORT })
     console.log(datasource.driver.version)
